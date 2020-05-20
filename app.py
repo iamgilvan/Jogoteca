32cc2f30d1053838b641ece90b34cb11ad33805d
Gilvan Almeida <gilvan.praxedes@outlook.com>
2020-05-20 12:55:10 -0300

    Persistência com banco de dados.

--- app.py
+++ app.py
@@ -1,39 +1,19 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
+import MySQLdb
+from dao import JogoDao, UsuarioDao
+from models import Jogo, Usuario
 
 app = Flask(__name__)
 app.secret_key = 'alura'
 
-# simples classe para exemplo
-class Jogo:
-    def __init__(self, nome, categoria, console):
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
+db = MySQLdb.connect(user='root', passwd='', host='127.0.0.1', port=3306, database='jogoteca')
 
-
-class Usuario:
-    def __init__(self, id, nome, senha):
-        self.id = id
-        self.nome = nome
-        self.senha = senha
-
-
-usuario1 = Usuario('luan', 'Luan Marques', '1234')
-usuario2 = Usuario('nico', 'Nico Steppat', '7a1')
-usuario3 = Usuario('flavio', 'Flávio', 'javascript')
-
-usuarios = { usuario1.id: usuario1,
-             usuario2.id: usuario2,
-             usuario3.id: usuario3 }
-
-jogo1 = Jogo('Super Mario', 'Acao', 'SNES')
-jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
-lista = [jogo1, jogo2, jogo3]
-
+jogo_dao = JogoDao(db)
+usuario_dao = UsuarioDao(db)
 
 @app.route('/')
 def index():
+    lista = jogo_dao.listar()
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
@@ -50,7 +30,7 @@ def criar():
     categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
-    lista.append(jogo)
+    jogo_dao.salvar(jogo)
     return redirect(url_for('index'))
 
 
@@ -62,14 +42,14 @@ def login():
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if request.form['usuario'] in usuarios:
-        usuario = usuarios[request.form['usuario']]
+    usuario = usuario_dao.buscar_por_id(request.form['usuario'])
+    if usuario:
         if usuario.senha == request.form['senha']:
             session['usuario_logado'] = usuario.id
             flash(usuario.nome + ' logou com sucesso!')
             proxima_pagina = request.form['proxima']
             return redirect(proxima_pagina)
-    else:
+    else :
         flash('Não logado, tente de novo!')
         return redirect(url_for('login'))
 
