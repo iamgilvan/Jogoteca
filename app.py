9c44c359da5fedc4554e33f8da79cfd3ac54073a
Gilvan Almeida <gilvan.praxedes@outlook.com>
2020-05-20 19:04:43 -0300

    Arrumando o projeto

--- app.py
+++ app.py
@@ -1,124 +1,12 @@
-import os
-import time
-
-from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
+from flask import Flask
 import MySQLdb
-from dao import JogoDao, UsuarioDao
-from models import Jogo, Usuario
 
 app = Flask(__name__)
-app.secret_key = 'alura'
-app.config['UPLOAD_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
+app.config.from_pyfile('config.py')
 
-db = MySQLdb.connect(user='root', passwd='', host='127.0.0.1', port=3306, database='jogoteca')
+db = MySQLdb.connect(user=app.config.get("USER"), host=app.config.get("HOST"),  port=app.config.get("PORT"), database=app.config.get("DATABASES"))
 
-jogo_dao = JogoDao(db)
-usuario_dao = UsuarioDao(db)
-
-@app.route('/')
-def index():
-    lista = jogo_dao.listar()
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
-
-
-@app.route('/novo')
-def novo():
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo='Novo jogo')
-
-
-@app.route('/criar', methods=['POST',])
-def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    jogo = jogo_dao.salvar(jogo)
-
-    arquivo = request.files['arquivo']
-    upload_path = app.config['UPLOAD_PATH']
-    timestamp = time.time()
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
-    return redirect(url_for('index'))
-
-
-@app.route('/login')
-def login():
-    proxima = request.args.get('proxima')
-    return render_template('login.html', proxima=proxima)
-
-
-@app.route('/autenticar', methods=['POST', ])
-def autenticar():
-    usuario = usuario_dao.buscar_por_id(request.form['usuario'])
-    if usuario:
-        if usuario.senha == request.form['senha']:
-            session['usuario_logado'] = usuario.id
-            flash(usuario.nome + ' logou com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
-    else:
-        flash('Não logado, tente denovo!')
-        return redirect(url_for('login'))
-
-
-@app.route('/atualizar', methods=['POST',])
-def atualizar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console, id=request.form['id'])
-    jogo_dao.salvar(jogo)
-
-    arquivo = request.files['arquivo']
-    upload_path = app.config['UPLOAD_PATH']
-    timestamp = time.time()
-    deleta_arquivo(jogo.id)
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
-    return redirect(url_for('index'))
-
-
-@app.route('/editar/<int:id>', methods=['GET',])
-def editar(id):
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('editar')))
-    jogo = jogo_dao.busca_por_id(id)
-    nome_imagem = recupera_imagem(id)
-    capa_jogo = f'capa{id}.jpg'
-    return render_template('editar.html', titulo='Editando jogo', jogo=jogo,
-                           capa_jogo=nome_imagem)
-
-
-@app.route('/deletar/<int:id>')
-def deletar(id):
-    jogo_dao.deletar(id)
-    flash('O jogo foi removido com sucesso!')
-    return redirect(url_for('index'))
-
-
-@app.route('/logout')
-def logout():
-    session['usuario_logado'] = None
-    flash('Nenhum usuário logado!')
-    return redirect(url_for('index'))
-
-
-@app.route('/uploads/<nome_arquivo>')
-def imagem(nome_arquivo):
-    return send_from_directory('uploads', nome_arquivo)
-
-
-def recupera_imagem(id):
-    for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
-        if f'capa{id}' in nome_arquivo:
-            return nome_arquivo
-
-
-def deleta_arquivo(id):
-    arquivo = recupera_imagem(id)
-    os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
-
+from views import  *
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+SECRET_KEY = 'alura'
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
+USER ='root'
+HOST='127.0.0.1'
+PORT=3306
+DATABASES='jogoteca'
